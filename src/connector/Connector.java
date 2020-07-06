fd28b2d7ea30b9683be941e5c902f7e784fb6263
jjmz1106 <66933069+jjmz1106@users.noreply.github.com>
2020-07-06 13:10:23 +0900

    Update Connector.java

--- src/connector/Connector.java
+++ src/connector/Connector.java
@@ -2,17 +2,22 @@ package connector;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connector {
-	private static final String url = "jdbc:mariadb://localhost:3306/java";
-	private static final String user = "test";
+//	private static final String url = "jdbc:mariadb://localhost:3306/java";  //마리아db
+	private static final String url = "jdbc:oracle:thin:@localhost:1522/xe"; //오라클
+//	private static final String user = "test"; //마리아db
+	private static final String user = "c##test"; //오라클
 	private static final String password = "test";
 	private static Connection conn;
 	
 	private static void init() {		
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+//			Class.forName("org.mariadb.jdbc.Driver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 		}catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -39,6 +44,15 @@ public class Connector {
 		}
 		conn = null;
 	}
+	public static void main(String[] args) throws SQLException {
+		Connection con = getConnection();
+		Statement stmt = con.createStatement();
+		String sql = "select * from board";
+		ResultSet rs = stmt.executeQuery(sql);
+		while(rs.next()) {
+			System.out.println("출력");
+		}
 
+	}
 	
 }
